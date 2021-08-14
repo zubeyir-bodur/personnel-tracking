5770a1b9d777be9a1ab6a386e0fdd58ed01992e4
Zübeyir Bodur <zubeyir99@hotmail.com>
2021-08-14 18:15:59 +0300

    Reference handling for lazy loading

--- personnel-tracking-webapi/Startup.cs
+++ personnel-tracking-webapi/Startup.cs
@@ -25,7 +25,9 @@ namespace personnel_tracking_webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => 
+                options.SerializerSettings.ReferenceLoopHandling = 
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddCors(policy => policy.AddDefaultPolicy(options => {
                 options.AllowCredentials().AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(origin => true);
             }));
