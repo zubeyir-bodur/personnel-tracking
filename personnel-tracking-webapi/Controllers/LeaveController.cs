76d12dfa1f2bd9b98d0970b4288bc994463e9291
Lenovo <laramerdol@gmail.com>
2021-08-18 12:48:05 +0300

    Add update and add function

--- personnel-tracking-webapi/Controllers/LeaveController.cs
+++ personnel-tracking-webapi/Controllers/LeaveController.cs
@@ -22,110 +22,136 @@ namespace personnel_tracking_webapi.Controllers
                 if (dbContext == null) dbContext = new PersonnelTrackingDBContext();
             }
 
-            [HttpGet]
-            public IActionResult Get()
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ResponseModel response = new ResponseModel();
+
+            try
             {
-                ResponseModel response = new ResponseModel();
-
-                try
+                var leaveList = dbContext.Set<Leave>().ToList();
+                var leaveDTOlist = new List<LeaveDTO>();
+                for (int i = 0; i < leaveList.Count; i++)
                 {
-                    var leaveList = dbContext.Leaves.Where(u => true).ToList();
-                    response.Data = leaveList;
-                }
-                catch (Exception ex)
-                {
-                    response.HasError = true;
-                    response.ErrorMessage = ex.Message;
-                }
-
-                return Ok(response);
-            }
-
-            // TODO: Rearrange here later
-            [HttpPost]
-            public IActionResult Post(Leave leaveDto)
-            {
-                ResponseModel response = new ResponseModel();
-
-                try
-                {
-                    Leave newLeave = new Leave();
-                newLeave.LeaveId = leaveDto.LeaveId;
-                newLeave.PersonnelId = leaveDto.PersonnelId;
-                newLeave.LeaveStart = leaveDto.LeaveStart;
-                newLeave.LeaveEnd = leaveDto.LeaveEnd;
-
-                    dbContext.Add<Leave>(newLeave);
-
-                    response.Data = newLeave;
-                }
-                catch (Exception e)
-                {
-                    response.HasError = true;
-                    response.ErrorMessage = e.Message;
+                    leaveDTOlist.Add(new LeaveDTO
+                    {
+                        leaveId = leaveList[i].LeaveId,
+                        personnelName = dbContext.Personnel.Where(e => e.PersonnelId == leaveList[i].PersonnelId).FirstOrDefault().PersonnelName,
+                        personnelSurname = dbContext.Personnel.Where(e => e.PersonnelId == leaveList[i].PersonnelId).FirstOrDefault().PersonnelSurname,
+                        leaveStart = leaveList[i].LeaveStart,
+                        leaveEnd = leaveList[i].LeaveEnd,
+                    });
                 }
 
-                return Ok(response);
+                response.Data = leaveDTOlist;
+
             }
 
 
-            [HttpPut]
-            public IActionResult Put(Leave leaveDto)
+
+            catch (Exception ex)
             {
-                ResponseModel response = new ResponseModel();
-
-                try
-                {
-                Leave leave = dbContext.Leaves.Where(u => u.LeaveId == leaveDto.LeaveId).FirstOrDefault();
-                leave.LeaveId = leaveDto.LeaveId;
-                leave.PersonnelId = leaveDto.PersonnelId;
-                leave.LeaveStart = leaveDto.LeaveStart;
-                leave.LeaveEnd = leaveDto.LeaveEnd;
-                dbContext.Update<Leave>(leave);
-
-                }
-                catch (Exception e)
-                {
-                    response.HasError = true;
-                    response.ErrorMessage = e.Message;
-                }
-
-                return Ok(response);
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
             }
 
-            [HttpDelete]
-            public IActionResult Delete(Leave leaveDto)
-            {
-                ResponseModel response = new ResponseModel();
+            return Ok(response);
+        }
 
-                try
-            {
-                    Leave leave = dbContext.Leaves.Where(u => u.LeaveId == leaveDto.LeaveId).FirstOrDefault();
-                    dbContext.Leaves.Remove(leave);
-                }
-                catch (Exception e)
-                {
-                    response.HasError = true;
-                    response.ErrorMessage = e.Message;
-                }
+        [HttpPost]
+        public IActionResult Post(Leave leave)
+        {
+            ResponseModel response = new ResponseModel();
 
-                return Ok(response);
+            try
+            {
+                dbContext.Leaves.Add(leave);
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
             }
 
-            public IActionResult SaveChanges()
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Leave leave)
+        {
+            ResponseModel response = new ResponseModel();
+
+            try
             {
-                ResponseModel response = new ResponseModel();
-                try
+                if (leave != null)
                 {
+                    Leave leaveDeleted = dbContext.Set<Leave>().Where(x => x.LeaveId == leave.LeaveId).FirstOrDefault<Leave>();
+
+                    leaveDeleted.LeaveStart = leave.LeaveStart;
+                    leaveDeleted.LeaveEnd = leave.LeaveEnd;
+
+                    dbContext.Update<Leave>(leaveDeleted);
                     dbContext.SaveChanges();
                 }
-                catch (Exception e)
-                {
-                    response.HasError = true;
-                    response.ErrorMessage = e.Message;
-                }
-
-                return Ok(response);
             }
+            catch (Exception ex)
+            {
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+
+        public IActionResult Delete(Leave leave)
+        {
+            ResponseModel response = new ResponseModel();
+
+            try
+            {
+                Leave leaveDeleted = dbContext.Leaves.Where(x => x.LeaveId== leave.LeaveId).FirstOrDefault<Leave>();
+                dbContext.Leaves.Remove(leaveDeleted).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+   
+
+    public IActionResult SaveChanges()
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                response.HasError = true;
+                response.ErrorMessage = e.Message;
+            }
+
+            return Ok(response);
         }
     }
+    public class LeaveDTO
+    {
+        public int leaveId { get; set; }
+        public string personnelName { get; set; }
+        public string personnelSurname { get; set; }
+
+        public DateTime leaveStart { get; set; }
+        public DateTime leaveEnd { get; set; }
+    }
+
+
+}
