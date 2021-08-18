428cdfb7bccdef4cac3da0fd1b3d19e49a6daccf
Selbi Ereshova <ereshovaselbi@gmail.com>
2021-08-18 16:13:36 +0300

    Addede Zubeyir's personnelType controller

--- personnel-tracking-webapi/Controllers/PersonnelTypeController.cs
+++ personnel-tracking-webapi/Controllers/PersonnelTypeController.cs
@@ -14,8 +14,9 @@ namespace personnel_tracking_webapi.Controllers
     [Route("api/personnelType")]
     [ApiController]
     [TokenCheck]
-    public class PersonnelTypeController : Controller
+    public class PersonnelTypeController : ControllerBase
     {
+
         private readonly PersonnelTrackingDBContext dbContext;
         public PersonnelTypeController()
         {
@@ -23,7 +24,7 @@ namespace personnel_tracking_webapi.Controllers
         }
 
         /// <summary>
-        /// Gets all companies to show to the admin
+        /// Lists all the roles available
         /// </summary>
         /// <returns></returns>
         /// GET: http://localhost:5000/api/personnelType
@@ -33,8 +34,12 @@ namespace personnel_tracking_webapi.Controllers
             ResponseModel response = new ResponseModel();
             try
             {
-                var personnelTypeList = dbContext.PersonnelTypes.ToList();
-                response.Data = personnelTypeList;
+                var personnelTypeDTOList = dbContext.PersonnelTypes.Select(u => new
+                {
+                    u.PersonnelTypeId,
+                    u.PersonnelTypeName
+                }).ToList();
+                response.Data = personnelTypeDTOList;
             }
             catch (Exception ex)
             {
@@ -43,5 +48,118 @@ namespace personnel_tracking_webapi.Controllers
             }
             return Ok(response);
         }
+
+        /// <summary>
+        /// Registers a personnelType to the table
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post(PersonnelTypeDTO personnelTypeDto)
+        {
+            var response = new ResponseModel();
+            try
+            {
+                var newPersonnelType = new PersonnelType
+                {
+                    PersonnelTypeId = personnelTypeDto.PersonnelTypeId,
+                    PersonnelTypeName = personnelTypeDto.PersonnelTypeName
+                };
+                dbContext.Add<PersonnelType>(newPersonnelType).State = EntityState.Added;
+                Console.WriteLine(dbContext.SaveChanges() + " rows affected.");
+            }
+            catch (Exception ex)
+            {
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
+                return NotFound(response);
+            }
+
+            response.Data = dbContext.PersonnelTypes.Select(u => new {
+                u.PersonnelTypeId,
+                u.PersonnelTypeName
+            }).ToList();
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Edits a personnelType row provided that the form is submitted
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Put(PersonnelTypeDTO personnelTypeDto)
+        {
+            var response = new ResponseModel();
+            try
+            {
+                var newPersonnelType = new PersonnelType
+                {
+                    PersonnelTypeId = personnelTypeDto.PersonnelTypeId,
+                    PersonnelTypeName = personnelTypeDto.PersonnelTypeName
+                };
+                dbContext.Update<PersonnelType>(newPersonnelType).State = EntityState.Modified;
+                Console.WriteLine(dbContext.SaveChanges() + " rows affected.");
+            }
+            catch (Exception ex)
+            {
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
+                return NotFound(response);
+            }
+
+            response.Data = dbContext.PersonnelTypes.Select(u => new {
+                u.PersonnelTypeId,
+                u.PersonnelTypeName
+            }).ToList();
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Deletes the personnelType from the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete(PersonnelTypeDTO personnelTypeDto)
+        {
+            ResponseModel response = new ResponseModel();
+
+            try
+            {
+                var personnelType = dbContext.PersonnelTypes.FirstOrDefault(u => u.PersonnelTypeId == personnelTypeDto.PersonnelTypeId);
+                dbContext.PersonnelTypes.Remove(personnelType).State = EntityState.Deleted;
+                Console.WriteLine(dbContext.SaveChanges() + " rows affected.");
+            }
+            catch (Exception e)
+            {
+                response.HasError = true;
+                response.ErrorMessage = e.Message;
+            }
+            response.Data = dbContext.PersonnelTypes.Select(u => new {
+                u.PersonnelTypeId,
+                u.PersonnelTypeName
+            }).ToList();
+            return Ok(response);
+        }
+
+        /* No wizard base admin page
+        public IActionResult SaveChanges()
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                response.HasError = true;
+                response.ErrorMessage = e.Message;
+            }
+            return Ok(response);
+        }*/
+    }
+
+    public class PersonnelTypeDTO
+    {
+        public int PersonnelTypeId { get; set; }
+        public string PersonnelTypeName { get; set; }
     }
 }
